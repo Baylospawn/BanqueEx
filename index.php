a4760a18ee715245fe5b6f67390e029c93b5c1f5
Baylospawn <microsoftbaylo@laposte.net>
2024-03-08 22:14:20 +0100

    Autoloader

--- index.php
+++ index.php
@@ -1,22 +1,21 @@
 <?php
 
+use App\Autoloader;
 use App\Client\Compte as CompteClient;
 use App\Banque\{CompteCourant, CompteEpargne};
 
-require_once './classes/Banque/Compte.php'; /**(dirname(__FILE__) .""); Pour un chemin absolu sur les gros projets*/
-require_once './classes/Banque/CompteCourant.php';
-require_once './classes/Banque/CompteEpargne.php';
-require_once './classes/Client/Compte.php';
+require_once 'classes/Autoloader.php';
+Autoloader::register();
 
 // On va instancier le compte
 $compte1 = new CompteCourant('Benoit', 500, 200);
 var_dump($compte1); 
 
-$compteEpargne = new CompteEpargne('Benoit',200, 10); 
-var_dump($compteEpargne);
+//$compteEpargne = new CompteEpargne('Benoit',200, 10); 
+//var_dump($compteEpargne);
 
-$compteEpargne->verserInterets();
-var_dump($compteEpargne);
+//$compteEpargne->verserInterets();
+//var_dump($compteEpargne);
 
-$client = new CompteClient;
-var_dump($client);
+//$client = new CompteClient;
+//var_dump($client);
